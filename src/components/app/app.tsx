4697f5724afb9df38a7f9b07b481c024c4203633
Alex Orlanski <alex.orleanski@gmail.com>
2024-01-28 01:02:56 +0300

    fix-review-final

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,13 +3,12 @@ import {FC, useState, useEffect} from 'react';
 import AppHeader from '../app-header/app-header'
 import BurgerIngrediets from '../burger-ingredients/burger-ingredients'
 import BurgerConstructor from '../burger-constructor/burger-constructor'
+import {TIngredientItem} from '../../utils/types';
 
 import styles from './app.module.css'
 
-import appOrder from '../../utils/order'
-import { TIngredientItem } from '../../utils/types';
 import {getIngredients} from '../../utils/api'
-
+import appOrder from '../../utils/order'
 
 const App:FC = () => {  
   
